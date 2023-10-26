f991f289942df38e493c6a3736887cfe6fd29da8
smAglo360 <smaglo360@gmail.com>
2023-10-26 20:44:38 +0300

    Preparations for app

---
+++ app/src/main/java/com/smaglo360/compositiongame/presentation/GameResult.kt
@@ -0,0 +1,16 @@
+package com.smaglo360.compositiongame.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.smaglo360.compositiongame.R
+class GameResult : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_game_result, container, false)
+    }
+}
--- app/src/main/java/com/smaglo360/compositiongame/presentation/MainActivity.kt
+++ app/src/main/java/com/smaglo360/compositiongame/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.smaglo360.compositiongame
+package com.smaglo360.compositiongame.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.smaglo360.compositiongame.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
