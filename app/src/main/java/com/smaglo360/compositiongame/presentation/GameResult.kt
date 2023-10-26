08b801b4d0bccf82d4ea4984b4a7e22950c45119
smAglo360 <smaglo360@gmail.com>
2023-10-26 22:52:55 +0300

    ViewBindings

--- app/src/main/java/com/smaglo360/compositiongame/presentation/GameResult.kt
+++ app/src/main/java/com/smaglo360/compositiongame/presentation/GameResult.kt
@@ -6,11 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.smaglo360.compositiongame.R
+import com.smaglo360.compositiongame.databinding.FragmentGameResultBinding
+import com.smaglo360.compositiongame.databinding.FragmentLevelOptionsBinding
+
 class GameResult : Fragment() {
+    private var _binding: FragmentGameResultBinding? = null
+    private val binding: FragmentGameResultBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameResultBinding == null")
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_game_result, container, false)
+    ): View {
+        _binding = FragmentGameResultBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
